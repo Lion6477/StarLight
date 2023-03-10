1c972b5ca8793af7903894bf226bbfd25e6c52db
Serhii <sekret7d@gmail.com>
2023-03-10 21:02:04 +0100

    Initial commit

---
+++ src/main/java/com/example/starlight/StarLightApplication.java
@@ -0,0 +1,13 @@
+package com.example.starlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarLightApplication.class, args);
+    }
+
+}
