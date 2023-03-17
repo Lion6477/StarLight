f9c79b87676f5da50daea76768b48dc907549b08
KateySt <kateystetsenko@gmail.com>
2023-03-17 23:32:39 +0200

    working case

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Maksym Khudoliy','zaxaqueiboigreu-5997@gmail.com',30,'photo1');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Alex Lee','faulleureheiyei-5854@gmail.com',26,'photo2');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Brandon Hernandez','merekucrautta-6463@gmail.com',17,'photo3');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Christopher Miller','quozilejama-8415@gmail.com',24,'photo4');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Dillon Rodrigues','neiquatrereucru-2403@gmail.com',20,'photo5');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Josh Smith','jaxifisammi-2877@gmail.com',35,'photo6');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Jotaro Kujo','hitubowixa-9787@gmail.com',17,'photo7');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Ryan White','troigriheuraque-2668@gmail.com',41,'photo8');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('Tyler Moore','gridippeiwace-9072@gmail.com',27,'photo9');
-INSERT INTO talent_entity (full_name,mail,age,avatar_url)
-VALUES ('William Moore','hounoimottauque-3830@gmail.com',35,'photo10');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Maksym Khudoliy','zaxaqueiboigreu-5997@gmail.com','111',30,'photo1');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Alex Lee','faulleureheiyei-5854@gmail.com','111',26,'photo2');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Brandon Hernandez','merekucrautta-6463@gmail.com','111',17,'photo3');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Christopher Miller','quozilejama-8415@gmail.com','111',24,'photo4');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Dillon Rodrigues','neiquatrereucru-2403@gmail.com','111',20,'photo5');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Josh Smith','jaxifisammi-2877@gmail.com','111',35,'photo6');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Jotaro Kujo','hitubowixa-9787@gmail.com','111',17,'photo7');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Ryan White','troigriheuraque-2668@gmail.com','111',41,'photo8');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('Tyler Moore','gridippeiwace-9072@gmail.com','111',27,'photo9');
+INSERT INTO talent_entity (full_name,mail,password,age,avatar_url)
+VALUES ('William Moore','hounoimottauque-3830@gmail.com','111',35,'photo10');
 
 INSERT INTO position_entity (position,talent_id,short_description,full_description)
 VALUES ('god',1,'i am Maksym :)','111111111111111111111111111111');
@@ -36,6 +36,6 @@ VALUES ('typescript',7,'vbghfcgjnf','111111111111111111111111111111');
 INSERT INTO position_entity (position,talent_id,short_description,full_description)
 VALUES ('data engineer',8,'vbghfcgjnf','111111111111111111111111111111');
 INSERT INTO position_entity (position,talent_id,short_description,full_description)
-VALUES ('engineer',9,'vbghfcgjnf','111111111111111111111111111111');
+VALUES ('photograph',9,'vbghfcgjnf','111111111111111111111111111111');
 INSERT INTO position_entity (position,talent_id,short_description,full_description)
-VALUES ('photograph',10,'vbghfcgjnf','111111111111111111111111111111');
+VALUES ('java',10,'vbghfcgjnf','111111111111111111111111111111');
