71ab915c9a5748d537f150aa68c00d8c3950ecf2
KateySt <kateystetsenko@gmail.com>
2023-03-19 18:53:27 +0200

    creat schema and controller for pagination

--- src/main/java/com/example/starlight/talent/TalentController.java
+++ src/main/java/com/example/starlight/talent/TalentController.java
@@ -1,9 +1,7 @@
 package com.example.starlight.talent;
 
-import com.example.starlight.talent.model.entity.TalentEntity;
+import com.example.starlight.talent.model.response.TalentsPagination;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class TalentController {
-    TalentRepository talentRepository;
+    TalentService talentService;
 
     @GetMapping("/talent")
-    public Page<TalentEntity>getAllTalents(
+    public TalentsPagination getAllTalents(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
-    ){
-        return talentRepository.findAll(PageRequest.of(page, size));
+    ) {
+        return talentService.talentsPagination(page, size);
     }
 }
---
+++ src/main/java/com/example/starlight/talent/model/response/TalentsPagination.java
@@ -0,0 +1,14 @@
+package com.example.starlight.talent.model.response;
+
+import com.example.starlight.talent.model.entity.TalentEntity;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record TalentsPagination(
+        int totalPage,
+        long totalTalents,
+        List<TalentEntity> talentShortDiscs
+) {
+}
